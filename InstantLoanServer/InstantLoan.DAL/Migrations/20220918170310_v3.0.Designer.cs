3670a48d481bb99b38c32c2ab9178745bf84e3c1
Verónica Cárdenas <veronica.cardenas@devsu.com>
2022-09-21 22:49:00 -0500

    avance servicios

--- InstantLoanServer/InstantLoan.DAL/Migrations/20220918170310_v3.0.Designer.cs
+++ InstantLoanServer/InstantLoan.DAL/Migrations/20220918170310_v3.0.Designer.cs
@@ -4,6 +4,7 @@ using InstantLoan.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InstantLoan.DAL.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220918170310_v3.0")]
+    partial class v30
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- InstantLoanServer/InstantLoan.DAL/Models/Client.cs
+++ InstantLoanServer/InstantLoan.DAL/Models/Client.cs
@@ -13,15 +13,19 @@ namespace InstantLoan.DAL.Models
 
         [Required]
         public string LastName { get; set; }
-        [Required]
-        public int Identification { get; set; }
+        
+        public string Identification { get; set; }
         [Required]
         public string Email { get; set; }
         [Required]
         public string Address { get; set; }
         [Required]
         public DateTime Birthday { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
 
+        [Required]
+        public string ProfileImage { get; set; }
 
     }
 }
